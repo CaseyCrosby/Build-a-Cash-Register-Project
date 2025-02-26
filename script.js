797642b3c958703f7b5d144c8bcd898c23cda4b0
Casey Crosby <caseycrozby@gmail.com>
2025-02-26 15:05:14 -0500

    css basic layout finished

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let price = 1.87;
 let cid = [
-  ['PENNY', 1.01],
-  ['NICKEL', 2.05],
-  ['DIME', 3.1],
-  ['QUARTER', 4.25],
-  ['ONE', 90],
-  ['FIVE', 55],
-  ['TEN', 20],
-  ['TWENTY', 60],
-  ['ONE HUNDRED', 100]
+  ['PENNIES', 1.01],
+  ['NICKELS', 2.05],
+  ['DIMES', 3.1],
+  ['QUARTERS', 4.25],
+  ['ONES', 90],
+  ['FIVES', 55],
+  ['TENS', 20],
+  ['TWENTIES', 60],
+  ['ONE HUNDREDS', 100]
 ];
 let cash = document.getElementById("cash");
 const changeDue = document.getElementById("change-due");
@@ -17,7 +17,7 @@ const total = document.getElementById("total");
 const register = document.getElementById("register");
 
 total.textContent =  `Total: $${price}`;
-register
+register.innerHTML = `${cid[0][0]}: $${cid[0][1]}<br>${cid[1][0]}: $${cid[1][1]}<br>${cid[2][0]}: $${cid[2][1]}<br>${cid[3][0]}: $${cid[3][1]}<br>${cid[4][0]}: $${cid[4][1]}<br>${cid[5][0]}: $${cid[5][1]}<br>${cid[6][0]}: $${cid[6][1]}<br>${cid[7][0]}: $${cid[7][1]}<br>${cid[8][0]}: $${cid[8][1]}`
 
 
 
