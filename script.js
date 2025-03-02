21c41e9c137440f005ee99c96df0e7ecd85081bd
Casey Crosby <caseycrozby@gmail.com>
2025-03-02 12:28:49 -0500

    changeFunction revise

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let price = 1.87;
 let cid = [
-  ['PENNIES', 1.01],
+  ['PENNIES', 1.01,],
   ['NICKELS', 2.05],
   ['DIMES', 3.1],
   ['QUARTERS', 4.25],
@@ -10,6 +10,9 @@ let cid = [
   ['TWENTIES', 60],
   ['ONE HUNDREDS', 100]
 ];
+let totalCid = cid.reduce((acc, arr) => acc += arr[1], 0);
+totalCid = parseFloat(totalCid.toFixed(2));
+console.log(totalCid);
 let cash = document.getElementById("cash");
 const changeDue = document.getElementById("change-due");
 const purchaseBtn = document.getElementById("purchase-btn");
@@ -17,23 +20,64 @@ const total = document.getElementById("total");
 const register = document.getElementById("register");
 
 total.textContent =  `Total: $${price}`;
-register.innerHTML = `${cid[0][0]}: $${cid[0][1]}<br>${cid[1][0]}: $${cid[1][1]}<br>${cid[2][0]}: $${cid[2][1]}<br>${cid[3][0]}: $${cid[3][1]}<br>${cid[4][0]}: $${cid[4][1]}<br>${cid[5][0]}: $${cid[5][1]}<br>${cid[6][0]}: $${cid[6][1]}<br>${cid[7][0]}: $${cid[7][1]}<br>${cid[8][0]}: $${cid[8][1]}`
+register.innerHTML = `${cid[0][0]}: $${cid[0][1]}<br>${cid[1][0]}: $${cid[1][1]}<br>${cid[2][0]}: $${cid[2][1]}<br>${cid[3][0]}: $${cid[3][1]}<br>${cid[4][0]}: $${cid[4][1]}<br>${cid[5][0]}: $${cid[5][1]}<br>${cid[6][0]}: $${cid[6][1]}<br>${cid[7][0]}: $${cid[7][1]}<br>${cid[8][0]}: $${cid[8][1]}`;
 
 
 
 
-  const cidFunction = (i, j) => {
-    for (let i = 0; i < cid.length; i++) {
-        for (let j = 0; j < cid[i].length; j++) {  
-        }
+ // const cidFunction = (i, j) => {
+ //   for (let i = 0; i < cid.length; i++) {
+ //       for (let j = 0; j < cid[i].length; j++) {  
+ //       }
+ //     }
+ //     return cid[i][j];
+ // };
+
+  let changeDueNumber = cash.value - price
+
+    const currencyUnits = {
+      "PENNIES": 0.01,
+      "NICKELS": 0.05,
+      "DIMES": 0.1,
+      "QUARTERS": 0.25,
+      "ONES": 1,
+      "FIVES": 5,
+      "TENS": 10,
+      "TWENTIES": 20,
+      "ONE HUNDREDS": 100
+    };
+
+    let change = [];
+    for (let i = cid.length - 1; i >= 0; i--) {
+      const currName = cid[i][0];
+      const currTotal = cid[i][1];
+      const currValue = currencyUnits[currName];
+      let currAmount = 0;
+
+      while (changeDueNumber >= currValue && currTotal > currAmount) {
+        changeDueNumber -= currValue;
+        changeDueNumber = parseFloat(changeDueNumber.toFixed(2)); // Prevent floating point errors
+        currAmount += currValue;
       }
-      return cid[i][j];
-  };
+
+      if (currAmount > 0) {
+        change.push([currName, currAmount]);
+      }
+      
+    }
+
+  
 
   const mainFunction = () => {
     if (cash.value == price) {
         changeDue.textContent = "No change due - customer paid with exact cash"
-    } 
+    } else if (cash.value < price) {
+      changeDue.textContent = "Customer does not have enough money to purchase the item"
+    } else if (price < cash.value && totalCid < changeDueNumber) {
+      changeDue.textContent = "Status: INSUFFICIENT_FUNDS"
+    } else  {
+      changeDue.textContent = `Status: OPEN ${change}`;
+    }
   };
 
 purchaseBtn.addEventListener("click", mainFunction);
@@ -41,5 +85,5 @@ purchaseBtn.addEventListener("click", mainFunction);
 
 
   //console.log(cid[i][j]);
-  console.log(cash)
-  console.log(cidFunction(3, 1));
+  console.log(changeDueNumber);
+  console.log(change[0]);
